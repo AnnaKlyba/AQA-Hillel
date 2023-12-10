61b4b8d444a462505cd943f4e082883144882b94
Анна Клыба <annaklyba@MacBook-Air-Anna.local>
2023-12-10 21:16:34 +0200

    My second homework

---
+++ HW3.py
@@ -0,0 +1,9 @@
+norway_text = ('Automatisering akselererer %syeblikket da roboter vil '
+               'erobre planeten v%sr. (%s)' % ('ø', 'å', 'Æ'))
+print(norway_text)
+norway_text = ('Automatisering akselererer {0}yeblikket da roboter vil '
+               'erobre planeten v{1}r. ({2})'.format('ø', 'å', 'Æ'))
+print(norway_text)
+norway_text = (f'Automatisering akselererer {"ø"}yeblikket da roboter vil '
+               f'erobre planeten v{"å"}r. ({"Æ"})')
+print(norway_text)
